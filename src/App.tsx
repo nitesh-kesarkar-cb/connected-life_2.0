65f0055d69cabfda9f40f88bb91312dbbafff49e
Nitesh Kesarkar <niteshkesarkar136@gmail.com>
2025-07-22 20:31:04 +0530

    Sign In and Sign Up components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
+
+
+import { useEffect } from 'react';
 import './App.css'
+import SignIn from './auth/pages/sign-in/SignIn'
+import React from 'react';
+import SignUp from './auth/pages/sign-up/SignUp';
 
 function App() {
+  const [location, setLocation] = React.useState(window.location.pathname);
 
+  useEffect(() => {
+
+   console.log('App component mounted', location);
+  }, []);
+  
   return (
     <>
-      <div>
-        Hello World
-        </div>
+
+    {
+     location === '/sign-up' ? <SignUp disableCustomTheme={false} /> : null
+    }
+
+    {
+      location !== '/sign-up' ? <SignIn disableCustomTheme={false} /> : null
+    }
+
+
     </>
   )
 }
